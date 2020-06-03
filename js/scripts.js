11acd936ab613b124b6e8c5c03c347f2266e8183
laloehm <laloehm1@gmail.com>
2020-06-03 11:44:59 -0500

    Ajustes en modo responsive

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,26 @@ icon.addEventListener("click", toggleClass);
 function toggleClass() {
     icon.classList.toggle("active");
 }
+
+let links = $('.nav-link');
+
+links.on('click', function () {
+    $('.collapse').removeClass('show');
+    $('.hamburguer-icon').removeClass('active');
+})
+
+/* Smooth scrolling para anclas */
+$("a.smooth").on("click", function (e) {
+    e.preventDefault();
+    var $link = $(this);
+    var anchor = $link.attr("href");
+    $("html, body").stop().animate({
+        scrollTop: $(anchor).offset().top - 100 + "px"
+    }, 1000);
+});
+
+$(function () {
+    $(document).bind("contextmenu", function (e) {
+        return false;
+    });
+});
